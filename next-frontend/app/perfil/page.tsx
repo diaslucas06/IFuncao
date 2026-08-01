e512786592d9df11d9dc52e8088a64a45a1c3cc3
diaslucas06 <dias.lucas1@escolar.ifrn.edu.br>
2026-08-01 18:46:27 -0300

    feat: adiciona a base inicial dos cards da página de perfil

--- next-frontend/app/perfil/page.tsx
+++ next-frontend/app/perfil/page.tsx
@@ -1,31 +1,53 @@
 import Header from "@/components/Header";
 import Profile from "@/components/Profile";
+import ButtonProfile from "@/components/ButtonProfile";
 
 export default function perfil() {
-    let lista_dados = [
-        'Nome',
-        'Nome',
-        'Nome',
-        'Nome',
-        'Nome',
-        'Nome'
-    ]
+    let dados = {
+        nome: 'Maria José dos Santos',
+        matricula: 20241101110000,
+        email: 'maria.jose@escolar.ifrn.edu.br',
+        ano_letivo: 3,
+        curso: 'Informática para Internet',
+        idade: 18,
+        media: 80
+    }
 
     return (
         <div>
             <Header/>
-            <Profile usuario={'Nome do usuário'} ano={3} matricula={20241101110000} curso={'Informática para Internet'} imagem={''}/>
+            <Profile usuario={'Nome do usuário'} ano={3} matricula={20241101110000} curso={'Informática para Internet'} idade={18} imagem={''}/>
             <div className="flex-col p-10 pt-40">
-                <div className="flex w-11/12">
-                    <div className="card-profile">
-                        <h1 className="font-bold text-[40px] ">Dados Pessoais</h1>
-                        {lista_dados.map((linhas, indice) => (
-                            <p className='' key={indice}>{linhas}</p>
-                        ))}
+                <div className="flex justify-between gap-10">
+                    <div className="card-profile" id="card-profile-data">
+                        <h1>Dados Pessoais</h1>
+                        <ul>
+                            <li><b>Nome completo:</b> {dados.nome}</li>
+                            <li><b>Matrícula:</b> {dados.matricula}</li>
+                            <li><b>E-mail:</b> {dados.email}</li>
+                            <li><b>Ano letivo:</b> {dados.ano_letivo}° ano</li>
+                            <li><b>Curso:</b> {dados.curso}</li>
+                            <li><b>Idade:</b> {dados.idade} anos</li>
+                        </ul>
                     </div>
-                    <div className="flex">
-                        
-                    </div> 
+                    <div className="card-profile">
+                        <h1>Seu progresso por ano</h1>
+                    
+                    </div>
+                    <div className="flex flex-col justify-between gap-4">
+                        <div className="card-profile" id="card-profile-media">
+                            <h1>Sua média</h1>
+                            <p>Média das notas em matemática do ano letivo: </p>
+                            <div>
+                                <p className="text-4xl text-(--profile-text-card-color) font-extrabold">{dados.media}</p>
+                            </div>
+                        </div>
+                        <div className="card-profile" id="card-profile-media">
+                            <ButtonProfile image={'/images/icons/pincel_dark.png'} text={'Editar'} callback={'/editarperfil'}/>
+                            <ButtonProfile image={'/images/icons/pincel_dark.png'} text={'Configurações'} callback={'/configuracoes'}/>
+                        </div>
+                    </div>
+                    
                 </div>
                
             </div>

b1859f06ee62d84650e3bf959965b9482f487892
diaslucas06 <dias.lucas1@escolar.ifrn.edu.br>
2026-07-31 23:04:32 -0300

    feat: importa e adiciona o ThemeProvider no layout para definir tema padrão

--- next-frontend/app/layout.tsx
+++ next-frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme_provider";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -26,9 +27,19 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${poppins.variable} ${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }

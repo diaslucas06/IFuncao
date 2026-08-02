bb6d25469b4135bed78b91c06496e5b0a04f0ee4
raissasantosx <raissa.medeiros@escolar.ifrn.edu.br>
2026-08-02 20:41:46 -0300

    Adiciona banco de dados

---
+++ backend/models.py
@@ -0,0 +1,110 @@
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, ForeignKey, Date, Text, JSON
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+db = create_engine("sqlite:///meubanco.db")
+Session = sessionmaker(bind=db)
+Base = declarative_base()
+
+class Usuario(Base):
+
+    __tablename__ = "usuarios" 
+
+    user_id = Column('user_id', Integer, primary_key=True, autoincrement=True, )
+    user_nome = Column('user_nome', String)
+    user_matricula = Column('user_matricula', Integer, unique=True, nullable=False)
+    user_email = Column('user_email', String)
+    user_ano_letivo = Column('user_ano_letivo', String)
+    user_curso = Column('user_curso', String)
+    user_data_nascimento = Column('user_data_nascimento', Date)
+    user_foto_url = Column('user_foto_url', String)
+    user_ofensiva_dias = Column('user_ofensiva_dias', Integer)
+
+    def __init__(self, user_nome, user_matricula, user_email, user_ano_letivo, user_curso, user_data_nascimento, user_foto_url, user_ofensiva_dias):
+        self.user_nome = user_nome
+        self.user_matricula = user_matricula
+        self.user_email = user_email
+        self.user_ano_letivo = user_ano_letivo
+        self.user_curso = user_curso
+        self.user_data_nascimento = user_data_nascimento
+        self.user_foto_url = user_foto_url
+        self.user_ofensiva_dias = user_ofensiva_dias
+
+class Questao(Base):
+
+    __tablename__ = "questoes" 
+
+    ques_id = Column('ques_id', Integer, primary_key=True, autoincrement=True)
+    ques_enunciado = Column('ques_enunciado', Text)
+    ques_alternativas = Column('ques_alternativas', JSON)
+    ques_etapa = Column('ques_etapa', String)
+    ques_nivel_dificuldade = Column('ques_nivel_dificuldade', String)
+    ques_cont_id = Column('ques_cont_id', ForeignKey('conteudos.cont_id'))
+    ques_resposta_correta = Column('ques_resposta_correta', String)
+    
+
+    def __init__(self, ques_enunciado, ques_alternativas, ques_etapa, ques_nivel_dificuldade, ques_cont_id, ques_resposta_correta):
+        self.ques_enunciado = ques_enunciado
+        self.ques_alternativas = ques_alternativas
+        self.ques_etapa = ques_etapa
+        self.ques_nivel_dificuldade = ques_nivel_dificuldade 
+        self.ques_cont_id = ques_cont_id
+        self.ques_resposta_correta = ques_resposta_correta
+
+class Conteudo(Base):
+
+    __tablename__ = "conteudos" 
+
+    cont_id = Column('cont_id', Integer, primary_key=True, autoincrement=True)
+    cont_nome = Column('cont_nome', String)
+    cont_etapa = Column('cont_etapa', String)
+
+    def __init__(self, cont_nome, cont_etapa):
+        self.cont_nome = cont_nome
+        self.cont_etapa = cont_etapa
+
+class Slide(Base):
+
+    __tablename__ = "slides" 
+
+    slid_id = Column('slid_id', Integer, primary_key=True, autoincrement=True)
+    slid_imagem_url = Column('slid_imagem_url', String)
+    slid_ordem = Column('slid_ordem', Integer)
+    slid_cont_id = Column('slid_cont_id', ForeignKey('conteudos.cont_id'))
+
+    def __init__(self, slid_imagem_url, slid_ordem, slid_cont_id):
+        self.slid_imagem_url = slid_imagem_url
+        self.slid_ordem = slid_ordem
+        self.slid_cont_id = slid_cont_id
+
+class Usuario_Conteudo(Base):
+
+    __tablename__ = "usuario_conteudos" 
+
+    usercont_id = Column('user_id', Integer, primary_key=True, autoincrement=True)
+    usercont_user_id = Column('usercont_user_id', ForeignKey('usuarios.user_id'))
+    usercont_cont_id = Column('usercont_cont_id', ForeignKey('conteudos.cont_id'))
+    usercont_concluido = Column('usercont_concluido', Boolean)
+
+    def __init__(self, usercont_user_id, usercont_cont_id, usercont_concluido):
+        self.usercont_user_id = usercont_user_id
+        self.usercont_cont_id = usercont_cont_id
+        self.usercont_concluido = usercont_concluido
+
+class Resposta_Questao(Base):
+
+    __tablename__ = "resposta_questoes" 
+
+    respques_id = Column('respques_id', Integer, primary_key=True, autoincrement=True)
+    respques_user_id = Column('respques_user_id', ForeignKey('usuarios.user_id'))
+    respques_ques_id = Column('respques_ques_id', ForeignKey('questoes.ques_id'))
+    respques_alternativa_escolhida = Column('respques_alternativa_escolhida', String)
+    respques_acertou = Column('respques_acertou', Boolean)
+    
+
+    def __init__(self, respques_user_id, respques_ques_id, respques_alternativa_escolhida, respques_acertou):
+        self.respques_user_id = respques_user_id
+        self.respques_ques_id = respques_ques_id
+        self.respques_alternativa_escolhida = respques_alternativa_escolhida
+        self.respques_acertou = respques_acertou
+
+Base.metadata.create_all(bind=db)

cbbccb2def43062898fde038bb3c9ad0d49175c4
diaslucas06 <dias.lucas1@escolar.ifrn.edu.br>
2026-08-03 16:01:18 -0300

    feat: adiciona a base das páginas de configurações

---
+++ next-frontend/app/perfil/configuracoes/seu_progresso/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+
+export default function seu_progresso() {
+    return (
+        <div>
+            <Header/>
+            <Sidebar/>
+        </div>
+    )
+}

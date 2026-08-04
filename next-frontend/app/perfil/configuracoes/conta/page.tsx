8b2f77cbafd3d1671736c6ee2084937d2f6a8a72
diaslucas06 <dias.lucas1@escolar.ifrn.edu.br>
2026-08-03 21:33:42 -0300

    feat: adiciona a página de conta nas configurações

--- next-frontend/app/perfil/configuracoes/conta/page.tsx
+++ next-frontend/app/perfil/configuracoes/conta/page.tsx
@@ -1,11 +1,55 @@
-import Header from "@/components/Header";
-import Sidebar from "@/components/Sidebar";
+'use client'
 
-export default function conta() {
+import Link from "next/link";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Sidebar from "@/components/Sidebar";
+import { ConfDiv, ConfH1, ConfH2, ConfCard, ConfInfoLi } from "@/components/ConfComponents";
+
+export default function informacoes_basicas() {
+
+    let dados = {
+        'nome': 'Maria José dos Santos',
+        'matricula': 20241101110000,
+        'email': 'maria.jose@escolar.ifrn.edu.br',
+        'ano_letivo': 3,
+        'curso': 'Informática para Internet',
+        'idade': 18,
+        'media': 80
+    }
+
     return (
         <div>
             <Header/>
-            <Sidebar/>
+            <div className="flex">
+                <Sidebar/>
+                <ConfDiv>
+                    <ConfH1>
+                        Conta
+                    </ConfH1>
+                    <div className="flex flex-col gap-3">
+                        <ConfCard>
+                            <ConfH2>Informações Pessoais</ConfH2>
+                            <ul className="flex flex-col gap-2">
+                                <ConfInfoLi><b className="font-semibold">Nome completo:</b> {dados.nome}</ConfInfoLi>
+                                <ConfInfoLi><b className="font-semibold">Matrícula:</b> {dados.matricula}</ConfInfoLi>
+                                <ConfInfoLi><b className="font-semibold">E-mail:</b> {dados.email}</ConfInfoLi>
+                                <ConfInfoLi><b className="font-semibold">Ano letivo:</b> {dados.ano_letivo}° ano</ConfInfoLi>
+                                <ConfInfoLi><b className="font-semibold">Curso:</b> {dados.curso}</ConfInfoLi>
+                            </ul>
+                        </ConfCard>
+                        <ConfCard>
+                            <ConfH2>Opções</ConfH2>
+                            <div className="flex flex-col gap-2">
+                                <Link className="font-poppins-sans font-semibold text-[24px] underline text-(--link-color)" href={''}>Desconectar do SUAP</Link>
+                                <Link className="font-poppins-sans font-semibold text-[24px] underline text-(--link-color)" href={''}>Sair do IFunção</Link>
+                                <Link className="font-poppins-sans font-semibold text-[24px] underline text-(--link-color)" href={''}>Apagar histórico de conteúdos vistos</Link>
+                            </div>
+                        </ConfCard>
+                    </div>
+                </ConfDiv>
+            </div>
+            <Footer/>
         </div>
     )
 }

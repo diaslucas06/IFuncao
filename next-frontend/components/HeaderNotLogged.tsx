92307e3fcfd22ef68f0eb99105faad130b1d0fae
diaslucas06 <dias.lucas1@escolar.ifrn.edu.br>
2026-08-06 18:05:59 -0300

    feat: finaliza a responsividade da header

--- next-frontend/components/HeaderNotLogged.tsx
+++ next-frontend/components/HeaderNotLogged.tsx
@@ -5,13 +5,14 @@ import { usePathname } from 'next/navigation'
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Book, Home, LogIn, CircleX, Menu } from "lucide-react";
 
 export default function HeaderNotLogged() {
     const nome_do_caminho = usePathname()
     const {theme, setTheme} = useTheme()
     const [montada, setMontada] = useState(false)
     const [menuAberto, setMenuAberto] = useState(false)
+    const [menuTemaAberto, setMenuTemaAberto] = useState(false)
     
     useEffect(() => {
         setMontada(true)
@@ -26,36 +27,75 @@ export default function HeaderNotLogged() {
     }
 
     return (
-        <header className="flex justify-between items-center py-5 px-10 bg-(--primary-700) text-(--neutral-0)">
-            <Image src={'/logo.png'} alt="Logo" width={225} height={65}/>
-            <div className="flex justify-between items-center gap-20">
-                <Link href={'/'} className={verificar_caminho('/') ? 'text-2xl hover:font-medium bg-white rounded-md px-4 py-2 text-(--primary-700)': 'text-2xl hover:font-medium'}>Início</Link>
-                <Link href={'/conteudos'} className={verificar_caminho('/conteudos') ? 'text-2xl hover:font-medium bg-white rounded-md px-4 py-2 text-(--primary-700)': 'text-2xl hover:font-medium'}>Conteúdos</Link>
-                <Link href={'/login'} className={verificar_caminho('/login') ? 'text-2xl hover:font-medium bg-white rounded-md px-4 py-2 text-(--primary-700)': 'text-2xl hover:font-medium'}>Login</Link>
-                <div className='flex gap-4 items-center relative cursor-pointer' onClick={() => setMenuAberto(!menuAberto)}>
+        <header className="sticky top-0 z-50 flex justify-between items-center bg-(--primary-700) text-(--neutral-0) shadow-xl py-2.5 px-5 md:py-5 md:px-10 md:gap-40 lg:gap-55 lp:gap-90 pc:gap-150">
+            <div className="flex md:hidden" onClick={() => setMenuAberto(!menuAberto)}>
+                {menuAberto ? <CircleX/>:<Menu/> }
+            </div>
+            {menuAberto && (
+                <ul className="absolute group-hover:block bg-(--menu-color) ofset-0 top-full mt-5 left-5 right-5 rounded-lg">
+                    <li className="flex justify-between hover:bg-(--primary-900) rounded-md">
+                        <Link href={'/'} className='flex gap-5 p-5 hover:cursor-pointer'>
+                            <Home size={25}/>
+                            <p>Início</p>
+                        </Link>
+                    </li>
+                    <hr />
+                    <li className="flex justify-between hover:bg-(--primary-900) rounded-md">
+                        <Link href={'/'} className='flex gap-5 p-5 hover:cursor-pointer'>
+                            <Book size={25}/>
+                            <p>Conteúdos</p>
+                        </Link>
+                    </li>
+                    <hr />
+                    <li className="flex justify-between hover:bg-(--primary-900) rounded-md">
+                        <button className='flex gap-5 p-5 hover:cursor-pointer' onClick={() => {theme === 'dark' ? setTheme('light'): setTheme('dark')}}>
+                            {theme === 'dark' ? <Moon size={25}/>: <Sun size={25}/>}
+                            <p>Altere o tema do site</p>
+                        </button>
+                    </li>
+                </ul>
+            )}
+            <Image className="h-auto w-[130px] md:w-[150px] lg:w-[200px] lp:w-[225px]" src={'/logo.png'} alt="Logo" width={225} height={65}/>
+            <Link href={'/login'} className={verificar_caminho('/login') ? 'flex gap-3 items-center hover:font-medium bg-white rounded-md px-4 py-2 text-(--primary-700) md:hidden md:text-lg lg:text-xl lp:text-2xl': 'flex gap-3 items-center hover:font-medium md:hidden md:text-lg lg:text-xl lp:text-2xl'}>
+                {verificar_caminho('/login') ? <LogIn className="md:w-[25px] lg:w-[40px]" color="var(--primary-700)" size={25} />: <LogIn className="md:w-[25px] lg:w-[40px]" size={25}/>}
+            </Link>
+            <div className="hidden flex-1 justify-between items-center md:flex">
+                <Link href={'/'} className={verificar_caminho('/') ? 'hover:font-medium bg-white rounded-md px-4 py-2 text-(--primary-700) md:text-lg lg:text-xl lp:text-2xl': 'hover:font-medium md:text-lg lg:text-xl lp:text-2xl'}>
+                    <Home className={verificar_caminho('/') ? 'md:flex lg:hidden': 'md:flex lg:hidden'}/>
+                    <p className={verificar_caminho('/') ? 'md:hidden lg:flex hover:font-medium': 'md:hidden lg:flex'}>Início</p>
+                </Link>
+                <Link href={'/conteudos'} className={verificar_caminho('/conteudos') ? 'hover:font-medium bg-white rounded-md px-4 py-2 text-(--primary-700) md:text-lg lg:text-xl lp:text-2xl': 'hover:font-medium md:text-lg lg:text-xl lp:text-2xl'}>
+                    <Book className={verificar_caminho('/conteudos') ? 'md:flex lg:hidden': 'md:flex lg:hidden'}/>
+                    <p className={verificar_caminho('/conteudos') ? 'md:hidden lg:flex hover:font-medium': 'md:hidden lg:flex'}>Conteúdos</p>
+                </Link>
+                <Link href={'/login'} className={verificar_caminho('/login') ? 'flex gap-3 items-center hover:font-medium bg-white rounded-md px-4 py-2 text-(--primary-700) md:text-lg lg:text-xl lp:text-2xl': 'flex gap-3 items-center hover:font-medium md:text-lg lg:text-xl lp:text-2xl'}>
+                    {verificar_caminho('/login') ? <LogIn className="md:w-[25px] lg:w-[40px]" color="var(--primary-700)" size={30} />: <LogIn className="md:w-[25px] lg:w-[40px]" size={30}/>}
+                    <p className="hover:font-medium">Login</p>
+                </Link>
+                <div className='flex gap-4 items-center relative cursor-pointer' onClick={() => setMenuTemaAberto(!menuTemaAberto)}>
                     {/* a tela renderiza primeiro vazia e depois define o texto e o icone que vao aparecer de acordo com o tema que a pessoa escolheu */}
                     {montada ? (
                         <>
-                            {theme === 'dark' ? <Moon size={40}/>: <Sun size={40}/>}
-                            <p className="text-2xl">
+                            {theme === 'dark' ? <Moon className="md:w-[30px] lg:w-[35px]" size={40}/>: <Sun className="md:w-[30px] lg:w-[35px]" size={40}/>}
+                            <p className="md:hidden lp:flex text-2xl">
                                 {theme === 'dark' ? 'Modo Escuro': 'Modo Claro'}
                             </p>
                         </>
                     ) : (
                         <div></div>
                     )}
-                    {menuAberto && (
-                        <ul className="absolute rounded-md group-hover:block bg-black left-0 ofset-0 top-full mt-5">
+                    {menuTemaAberto && (
+                        <ul className="absolute rounded-md group-hover:block bg-black right-0 ofset-0 top-full mt-5">
                             <li className="flex justify-between hover:bg-(--primary-900) rounded-md">
-                                <button className='flex justify-between text-[20px] p-5 w-55 hover:cursor-pointer' onClick={() => setTheme('light')}>
-                                    Modo Claro
+                                <button className='flex justify-between p-5 hover:cursor-pointer w-45 lg:w-55' onClick={() => setTheme('light')}>
+                                    <p className="lg:text-[20px]">Modo Claro</p>
                                     <Sun size={25}/>
                                 </button>
                             </li>
                             <hr />
                             <li className="flex justify-between hover:bg-(--primary-900) rounded-md">
-                                <button className='flex justify-between text-[20px] p-5 w-55 hover:cursor-pointer' onClick={() => setTheme('dark')}>
-                                    Modo Escuro
+                                <button className='flex justify-between p-5 hover:cursor-pointer w-45 lg:w-55' onClick={() => setTheme('dark')}>
+                                    <p className="lg:text-[20px]">Modo Escuro</p>
                                     <Moon size={25}/>
                                 </button>
                             </li>

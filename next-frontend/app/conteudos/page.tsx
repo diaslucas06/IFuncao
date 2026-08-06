f576c6e0a9edcac6d3063244ede58a7275df8d2b
diaslucas06 <dias.lucas1@escolar.ifrn.edu.br>
2026-08-05 22:51:56 -0300

    feat: adiciona footer e usa variável para verificar se o usuário está logado

--- next-frontend/app/conteudos/page.tsx
+++ next-frontend/app/conteudos/page.tsx
@@ -1,9 +1,22 @@
 import Header from "@/components/Header";
+import HeaderNotLogged from "@/components/HeaderNotLogged";
+import Footer from "@/components/Footer";
+
+let logado = true
 
 export default function seuprogresso() {
+    if (logado) {
+        return (
+            <div>
+                <Header/>
+                <Footer/>
+            </div>
+        )
+    }
     return (
         <div>
-            <Header/>
+            <HeaderNotLogged/>
+            <Footer/>
         </div>
     )
 }
--- next-frontend/app/page.tsx
+++ next-frontend/app/page.tsx
@@ -1,9 +1,22 @@
 import HeaderNotLogged from "@/components/HeaderNotLogged";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+let logado = true
 
 export default function Home() {
+  if (logado) {
+    return (
+      <div>
+        <Header/>
+        <Footer/>
+      </div>
+    )
+  }
   return (
     <div>
       <HeaderNotLogged/>
+      <Footer/>
     </div>
   );
 }

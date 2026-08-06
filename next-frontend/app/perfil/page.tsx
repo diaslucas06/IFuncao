33c5704194b76f0d308db9ec93624742404fe9d5
diaslucas06 <dias.lucas1@escolar.ifrn.edu.br>
2026-08-05 22:54:17 -0300

    feat: completa a responsividade do perfil

--- next-frontend/app/perfil/page.tsx
+++ next-frontend/app/perfil/page.tsx
@@ -6,7 +6,7 @@ import Profile from "@/components/Profile";
 import ButtonProfile from "@/components/ButtonProfile";
 import BarraProgresso from "@/components/BarraProgresso";
 import { CardBackground, CardH1, CardLi, CardP } from "@/components/CardProfile";
-import CardConteudo from "@/components/CardConteudo";
+import CardHistorico from "@/components/CardHistorico";
 import { Settings, PencilSparkles, ChevronLeft, ChevronRight } from "lucide-react";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -16,14 +16,17 @@ export default function perfil() {
     const [conteudos_por_pagina, setConteudosPorPagina] = useState(4)
 
     useEffect(() => {
-        if (window.innerWidth === 1920) {
+        if (window.innerWidth <= 1920) {
             setConteudosPorPagina(4)
         } 
-        if (window.innerWidth === 1440) {
-            setConteudosPorPagina(4)
-        }
-        if (window.innerWidth === 1024) {
+        if (window.innerWidth <= 1024) {
             setConteudosPorPagina(3)
+        }
+        if (window.innerWidth <= 545) {
+            setConteudosPorPagina(2)
+        }
+        if (window.innerWidth <= 415) {
+            setConteudosPorPagina(historico.length)
         }
     }, [])
 
@@ -77,9 +80,9 @@ export default function perfil() {
         <div>
             <Header/>
             <Profile usuario={'Nome do usuário'} ano={3} matricula={20241101110000} curso={'Informática para Internet'} idade={18} imagem={''}/>
-            <main className="flex flex-col p-10 gap-7 lg:pt-28 lp:pt-30 pc:pt-40">
+            <main className="flex flex-col p-5 pt-10 gap-5 md:p-10 md:pt-20 lg:gap-7 lg:pt-28 lp:pt-30 pc:pt-40">
                 <div className="flex flex-1 flex-wrap justify-between gap-5">
-                    <CardBackground className="flex gap-[20px] lg:flex-1 pc:flex-1 pc:w-[47%]">
+                    <CardBackground className="flex gap-[20px] flex-1/1 lg:flex-1 pc:flex-1 pc:w-[47%]">
                         <CardH1>Dados Pessoais</CardH1>
                         <ul className="flex flex-col gap-2">
                             <CardLi><b className="font-semibold">Nome completo:</b> {dados.nome}</CardLi>
@@ -90,7 +93,7 @@ export default function perfil() {
                             <CardLi><b className="font-semibold">Idade:</b> {dados.idade} anos</CardLi>
                         </ul>
                     </CardBackground>
-                    <CardBackground className="flex gap-[20px] lg:flex-1 pc:flex-none pc:w-[25%]">
+                    <CardBackground className="flex gap-[20px] flex-1/1 lg:flex-1 pc:flex-none pc:w-[25%]">
                         <CardH1>Seu progresso por ano</CardH1>
                         <div>
                             <BarraProgresso ano={1} porcentagem={progresso.primeiro}/>
@@ -99,34 +102,34 @@ export default function perfil() {
                             <BarraProgresso ano={'Matemática Enem'} porcentagem={progresso["Matemática Enem"]}/>
                         </div>
                     </CardBackground>
-                    <div className="flex justify-between gap-4 lg:w-[100%] lg:flex-row pc:w-[25%] pc:flex-col">
-                        <CardBackground className="flex lg:gap-[20px] lg:flex-3 lp:flex-1 pc:gap-[5px]">
+                    <div className="flex flex-col justify-between gap-4 w-full md:flex-row lg:flex-row pc:w-[25%] pc:flex-col">
+                        <CardBackground className="flex flex-1 md:gap-[20px] lg:flex-3 lp:flex-1 pc:gap-[5px]">
                             <CardH1>Sua média</CardH1>
                             <CardP>Média das notas em matemática do ano letivo: </CardP>
                             <div>
-                                <p className="text-4xl text-(--profile-text-card-color) font-extrabold">{dados.media}</p>
+                                <p className="text-[20px] md:text-3xl lp:text-4xl text-(--profile-text-card-color) font-extrabold">{dados.media}</p>
                             </div>
                         </CardBackground>
-                        <CardBackground className="flex flex-1 justify-between">
-                            <ButtonProfile icon={<PencilSparkles size={35} color="var(--profile-button-icon-color)"/>} text={'Editar'} callback={'/editarperfil'}/>
-                            <ButtonProfile icon={<Settings size={35} color="var(--profile-button-icon-color)"/>} text={'Configurações'} callback={'/perfil/configuracoes/informacoes_basicas'}/>
+                        <CardBackground className="flex flex-1 gap-2 justify-around md:gap-0 lp:justify-between">
+                            <ButtonProfile icon={<PencilSparkles size={35} color="var(--profile-button-icon-color)" className="w-[25px] md:w-[35px]"/>} text={'Editar'} callback={'/editarperfil'}/>
+                            <ButtonProfile icon={<Settings size={35} color="var(--profile-button-icon-color)" className="w-[25px] md:w-[35px]"/>} text={'Configurações'} callback={'/perfil/configuracoes/informacoes_basicas'}/>
                         </CardBackground>
                     </div>
                 </div>
                 <div className="flex justify-between gap-5">
-                    <CardBackground className="w-[100%] gap-[20px]">
+                    <CardBackground className="w-full gap-[20px]">
                         <CardH1>Histórico de conteúdos acessados</CardH1>
-                        <div className="flex items-center w-[100%]">
+                        <div className="flex items-center w-full">
                             <button onClick={() => paginaAnterior()} disabled={inicioIndex === 0} className="disabled:opacity-30 transition-opacity">
-                                <ChevronLeft className='hover:cursor-pointer' size={40} color="var(--arrow-color)"/>
+                                <ChevronLeft className='hidden hover:cursor-pointer w-[20px] ml:flex ml:w-[25px] lg:w-[40px]' size={40} color="var(--arrow-color)"/>
                             </button>
-                            <div className="flex flex-wrap items-center justify-around w-[100%]">
+                            <div className="flex flex-col gap-2 flex-wrap items-center justify-around w-full ml:flex-row ml:gap-0">
                                 {conteudosExibidos.map((conteudo, index) => (
-                                    <CardConteudo key={conteudo.id} nome={conteudo.nome}  link={conteudo.link}  porcentagem={conteudo.porcentagem} index={index}/>
+                                    <CardHistorico key={conteudo.id} nome={conteudo.nome}  link={conteudo.link}  porcentagem={conteudo.porcentagem} index={index}/>
                                 ))}
                             </div>
                             <button onClick={() => proximaPagina()} disabled={inicioIndex + conteudos_por_pagina > historico.length} className="disabled:opacity-30 transition-opacity">
-                                <ChevronRight className='hover:cursor-pointer' size={40} color="var(--arrow-color)"/>
+                                <ChevronRight className='hidden hover:cursor-pointer w-[20px] ml:flex ml:w-[25px] lg:w-[40px]' size={40} color="var(--arrow-color)"/>
                             </button>
                          </div>
                     </CardBackground>

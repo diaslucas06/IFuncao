d4fa3ed20e5269120d161a0a51b4fb7d42098af5
Maria <santos.silva3@escolar.ifrn.edu.br>
2026-09-15 08:36:25 -0300

    feat: atualiza backend com rota de auth

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session, sessionmaker
 from typing import List
 
@@ -6,7 +7,15 @@ from models import db, Base, Usuario
 from schemas import UsuarioSchema
 from routes.auth_routes import auth_routes
 
-app = FastAPI(title="IFuncao API")
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Registra as rotas do grupo
 app.include_router(auth_routes)
